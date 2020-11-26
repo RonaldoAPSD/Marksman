0be8a43795171ce265a800640ddad52b6eea9430
Ronaldo Alvarez Perez <ronaldoapsd@outlook.com>
2020-11-25 19:21:10 -0800

    Added Logging Library
    Added a custom logging library using spdlog as the base, the engine will
    use a very abstract version of spdlogs logging system for easier debugging.

--- Marksman/src/Marksman.h
+++ Marksman/src/Marksman.h
@@ -3,6 +3,7 @@
 // FOR MARKSMAN APPLICATION USE
 
 #include "Marksman/Application.h"
+#include "Marksman/Log.h"
 
 // --------- ENTRYPOINT --------------
 #include "Marksman/EntryPoint.h"
--- Marksman/src/Marksman/EntryPoint.h
+++ Marksman/src/Marksman/EntryPoint.h
@@ -6,6 +6,10 @@ extern Marksman::Application* Marksman::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Marksman::Log::Init();
+	MK_CORE_WARN("Initialized Log!");
+	MK_INFO("Hello!");
+
 	auto app = Marksman::CreateApplication();
 	app->Run();
 	delete app;
